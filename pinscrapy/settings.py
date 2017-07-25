88bdad64c87812a6f8806ca96efe52b185a2a5e1
Viton Vitanis <viton.vitanis@gmail.com>
2017-07-25 12:29:27 +0300

    Modify settings

--- pinscrapy/settings.py
+++ pinscrapy/settings.py
@@ -30,7 +30,7 @@ CONCURRENT_REQUESTS = 32
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -40,6 +40,8 @@ COOKIES_ENABLED = False
 
 # Feed format
 FEED_FORMAT = 'jsonlines'
+FEED_URI = 'export.json'
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
